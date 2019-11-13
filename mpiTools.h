45d7c013fbb1bd1513fc5a673fdb2c8a95ad53e9
Manu <mcabrera@iaa.es>
2019-11-13 09:18:17 +0100

    remove unnecesary code

--- mpiTools.h
+++
@@ -1,5 +0,0 @@
-#include "defines.h"
-#include "mpi.h"
-
-//void buildMpiType( MPI_Datatype * mpiInitModel,int nlambda, MPI_Datatype * mpiVPixels, int N_SIG, int N_PI);
-void buildMpiType( MPI_Datatype * mpiInitModel);
