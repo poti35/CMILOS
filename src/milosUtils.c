e40a9f4b3ba5280e29e2a26bb41361351c62980b
Manuel Cabrera <44465130+poti35@users.noreply.github.com>
2020-04-21 09:01:52 +0200

    Update milosUtils.c

--- src/milosUtils.c
+++ src/milosUtils.c
@@ -671,7 +671,7 @@ int lm_mils(Cuantic *cuantic, PRECISION *wlines, PRECISION *lambda, int nlambda,
 	for(i=0;i<nlambda*NPARMS;i++){
 		if(spectro[i]<-1){ 
 			printf("\n sigma %i cambiada",i);
-			vSigma[i]=NOISE_GHOST*NOISE_GHOST;
+			vSigma[i]= 100.0*100.0;
 		}
 	}
 	
